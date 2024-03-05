ddf3521f10b1bddacdcfdae7b6f30ec7b7d47d0c
Elisei Codescu <elisei@gmail.com>
2024-03-05 15:05:33 +0200

    altecva

--- lab2 (2).c
+++ lab2 (2).c
@@ -3,18 +3,20 @@
 
 int function(int x){
 	x=x-2;
-	x=function(x);
-	return x;
+    if(x==0)
+        return x;
+        else
+        x=function(x);
 }
 
 int main(){
 	int a=30, i=0;
-	
+
 	while (a>0){
 		a=function(a);
 		i++;
 	}
 	printf("%d", i);
-	return 0;	
+	return 0;
 }
 
